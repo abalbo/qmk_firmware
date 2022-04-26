8751835ac450532de3ff8d69a55c02eaccf99bcd
abalbo <abalbo91@gmail.com>
2022-04-26 21:31:19 +0200

    left/right working

--- keyboards/handwired/keyblume/keyblume.c
+++ keyboards/handwired/keyblume/keyblume.c
@@ -4,5 +4,13 @@
 void board_init(void) {
     // B9 is configured as I2C1_SDA_PIN in the board file; that function must be
     // disabled before using B7 as I2C1_SDA.
-    setPinInputHigh(B9);
+    //setPinInputHigh(B9);
+}
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
 }
