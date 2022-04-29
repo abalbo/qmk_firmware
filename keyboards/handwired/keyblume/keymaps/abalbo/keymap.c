db412eceda682f326f3a5de0e82c932a6e02d417
abalbo <abalbo91@gmail.com>
2022-04-29 21:17:35 +0200

    add media keys and del

--- keyboards/handwired/keyblume/keymaps/abalbo/keymap.c
+++ keyboards/handwired/keyblume/keymaps/abalbo/keymap.c
@@ -13,17 +13,18 @@ enum Encoder {
 };
 
 enum CUSTOM_KEYCODES {
-    ALT_E = SAFE_RANGE
+    ALT_E = SAFE_RANGE,
+    M_NEXT_PREV,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_split70(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_INS,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_MPLY, M_NEXT_PREV, KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_MUTE, KC_BSPC, KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_ENT,  KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS, KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,   KC_END,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_ENT,  KC_END,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS, KC_PGUP,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_split70(
@@ -31,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ____,  ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____, ____,  ____, ____, ____,
         ____,  ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ALT_E, ____, ____,  ____,
         ____, ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,  ____, ____, ____,
-        ____, ____, ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____, ____,   ____, ____, ____,   ____, 
+        ____, ____, ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____, ____,   ____, ____, ____,   ____,
         ____, ____, ____, ____,  ____,  ____, ____, ____, ____, ____, ____
     ),
 
@@ -129,6 +130,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Do something else when release
       }
       return false; // Skip all further processing of this key
+    case M_NEXT_PREV:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LSFT)) {
+            unregister_code(KC_LSFT);
+            tap_code(KC_MPRV);
+            register_code(KC_LSFT);
+        }
+        else
+        {
+            tap_code(KC_MNXT);
+        }
+      }
+      return false;
     default:
       return true; // Process all other keycodes normally
   }
